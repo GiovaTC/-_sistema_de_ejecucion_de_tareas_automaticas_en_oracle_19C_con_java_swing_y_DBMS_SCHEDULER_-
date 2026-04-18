e2e602b38321a1a73ab9010c5454ff53c75827af
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-18 11:43:00 -0500

    commit - TareaService .

---
+++ src/service/TareaService.java
@@ -0,0 +1,12 @@
+package service;
+
+import dao.OracleDAO;
+
+public class TareaService {
+
+    private OracleDAO dao = new OracleDAO();
+
+    public void ejecutar() {
+        dao.ejecutarTarea();
+    }   
+}
